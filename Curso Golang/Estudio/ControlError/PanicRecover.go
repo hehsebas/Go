54d8ae5e09270e0a27fb6ee80b5c074b964bcfc9
hehsebas <sebasrre@gmail.com>
2024-08-19 16:11:32 -0500

    Cambio nombre

---
+++ Curso Golang/Estudio/ControlError/PanicRecover.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func divide(dividendo, divisor int) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	validateZero(divisor)
+	fmt.Println(dividendo / divisor)
+}
+
+func validateZero(divisor int) {
+	if divisor == 0 {
+		panic("No se puede dividir por cero.")
+	}
+}
+func main() {
+	divide(100, 10)
+	divide(200, 10)
+	divide(300, 0)
+	divide(400, 10)
+}
